75f3eaceb2464e5508269feda852b5dd80405224
nbqhuy <huynbqse181742@fpt.edu.vn>
2023-08-10 11:43:49 +0700

    fix audio

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 function App() {
 const audio1 = document.getElementById("audio-1");
 const audio2 = document.getElementById("audio-2");
-const audio3 = document.getElementById("audio-3");
 let rotation = 0;
 let isRotating = false;
 let rotationInterval;
@@ -13,15 +12,10 @@ function rotatePropeller() {
     const rotateButton = document.getElementById("rotateButton");
     audio1.pause()
     audio2.pause()
-    audio3.pause()
     audio1.currentTime = 0;
     audio2.currentTime = 0;
-    audio3.currentTime=0
     if (!isRotating) {
         audio1.play();
-        setTimeout(() => {
-    audio2.play();
-    }, audio1.duration * 1000);
     isRotating = true;
     rotateButton.textContent = "Stop";
     rotationInterval = setInterval(() => {
@@ -30,10 +24,8 @@ function rotatePropeller() {
     }, 20); 
   } else {
     audio1.pause()
-    audio2.pause()
-    audio3.play()
+    audio2.play()
     audio1.currentTime = 0;
-    audio2.currentTime = 0;
     clearInterval(rotationInterval); 
     isRotating = false;
     rotateButton.textContent = "Run"; 
